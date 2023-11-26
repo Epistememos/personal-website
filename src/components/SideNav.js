6b51c6dddce3bb08ae35d735c99c38acd6093f2d
Epistememos <signup.yy@gmail.com>
2023-11-26 18:15:15 -0500

    Added Images, changed robots.txt, arranged the dividers

--- src/components/SideNav.js
+++ src/components/SideNav.js
@@ -4,14 +4,14 @@ import "./SideNav.css";
 const SideNav = (props) => {
     return (
         <div className="sidenav-container">
-            <img src="/images/img-logo.jpg" alt="Pilot"/>
+            <img src="/assets/img-logo.jpg" alt="Pilot"/>
             <h1>{props.name}</h1>
-            <a href="/simplified">Simplified View</a>
+            <a href="/simplified">Simplified</a>
             <a href="#section">About</a>
             <a href="#section">Projects</a>
             <a href="#section">Involvement</a>
             <a href="#blog">Blog</a>
-            <a href="#section">Contact</a>
+            <a href="#section">Contact</a> 
         </div>
     );
 };
