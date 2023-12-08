e1f8f8852c8bbabaf607e76798f26c0bc3ef04b9
Epistememos <signup.yy@gmail.com>
2023-12-08 18:27:48 -0500

    Button Component

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,11 +1,16 @@
-import React from 'react'
+import React from 'react';
+import "./Button.css";
 
-function Button() {
+function Button(props) {
   return (
     <div>
-      
+      <button
+      className="btn btn-shadow btn-shadow--black"
+      onClick={() => window.open(props.path, '_blank')}>
+      <span>{props.name}</span>
+      </button>
     </div>
   )
 }
 
-export default Button
+export default Button;
--- src/components/SideNav.js
+++ src/components/SideNav.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./SideNav.css";
+import Button from "./Button";
 
 const SideNav = (props) => {
     return (
         <div className="sidenav-container">
             <img src="/assets/img-logo.jpg" alt="Pilot"/>
             <h1>{props.name}</h1>
+            <Button name="Resume" path="/assets/Resume.pdf"/>
             <a href="/simplified">Simplified</a>
             <a href="#section">About</a>
             <a href="#section">Projects</a>
