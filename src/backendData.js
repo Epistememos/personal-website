d6fecfcdae8a53897bf655a915125cdedccba4b7
Epistememos <signup.yy@gmail.com>
2024-02-17 15:29:02 -0500

    Updated backendData + 2 imgs

--- src/backendData.js
+++ src/backendData.js
@@ -9,7 +9,8 @@ const backendData = [
         },
         {
           title: 'TaskMaster',
-          src: '/assets/img-taskmaster.jpg'
+          src: '/assets/img-taskmaster.jpg',
+          path: 'https://github.com/Epistememos/TaskMaster'
         },
         {
           title: 'Projecto (March 2024)', 
@@ -22,18 +23,20 @@ const backendData = [
       id: '#involvements',
       categories: [
         {
-          title: 'MPMA',
+          title: 'Director of Outreach - MPMA',
           src: '/assets/img-mpma.jpg',
           path: 'https://www.linkedin.com/company/mpma-mcgill/'
         },
         {
-          title: 'TaskMaster',
-          src: '/assets/img-taskmaster.jpg',
-          path: 'https://github.com/Epistememos/TaskMaster'
+          title: 'Junior Consultant - ConnectUs',
+          src: '/assets/img-connectus.jpg',
+          path: 'https://www.instagram.com/mcgill.cu/?hl=en'
+          
         },
         {
-          title: 'Projecto (March 2024)', 
-          src: '/assets/img-projecto.jpg'
+          title: 'VP External - MSFC', 
+          src: '/assets/img-msfc.jpg',
+          path: 'https://www.instagram.com/mcgillflyingclub/?hl=en'
         }
       ]
     },
@@ -42,16 +45,16 @@ const backendData = [
       id: '#blog',
       categories: [
         {
-          title: 'Free Will',
+          title: 'Free Will (Coming Soon)',
           src: '/assets/img-mpma.jpg',
           path: 'https://www.linkedin.com/company/mpma-mcgill/'
         },
         {
-          title: 'Misunderstood Nietzsche',
+          title: 'Misunderstood Nietzsche (Soon)',
           src: '/assets/img-taskmaster.jpg'
         },
         {
-          title: 'Option Trading Simplified', 
+          title: 'Option Trading Simplified (Soon)', 
           src: '/assets/img-projecto.jpg'
         }
       ]
