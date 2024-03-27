b648670d4beaebce4eb1851de6e37be1f10f7287
Epistememos <signup.yy@gmail.com>
2024-03-27 00:42:11 -0400

    HashRouter rm Switch

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { HashRouter, Switch, Route } from 'react-router-dom';
+import { HashRouter, Route } from 'react-router-dom';
 import './App.css';
 import Home from './components/pages/Home';
 import Simplified from './components/pages/Simplified';
@@ -11,14 +11,12 @@ import Article from './components/pages/Article';
 function App() {
   return (
     <HashRouter>
-      <Switch>
         <Route exact path="/" component={Home} />
         <Route path="/simplified" component={Simplified} />
         <Route path="/projects" component={Projects} />
         <Route path="/involvements" component={Involvements} />
         <Route exact path="/blog" component={Blog} />
         <Route path="/blog/:id" component={Article} />
-      </Switch>
     </HashRouter>
   );
 }
